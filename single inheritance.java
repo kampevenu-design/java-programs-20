f583f38a375679b20efcc95d3e9ba4c41104a10c
venu <kampevenu@gmail.com>
2026-06-18 16:22:33 +0530

    Create single inheritance.java

---
+++ single inheritance.java
@@ -0,0 +1,17 @@
+class Animal {
+    void eat() {
+        System.out.println("Animal is Eating");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is Barking");
+    }
+
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+    }
+}
